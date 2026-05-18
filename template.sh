1659dd3232289e23b250594e78ab1efa84964127
varshalohakare <lohakarevarsha43@gmail.com>
2026-05-19 02:05:31 +0530

    web app completed

--- template.sh
+++ template.sh
@@ -1,17 +1,18 @@
 #creating directory using bash script
 
-mkdir -p src
+#mkdir -p src
 mkdir -p research
 
 #creating files using bash script
-touch src/__init__.py
-touch src/helper.py
-touch src/prompt.py
-touch .env
-touch setup.py
-touch app.py
+# touch src/__init__.py
+#touch src/helper.py
+#touch src/prompt.py
+#touch .env
+#touch setup.py
+#touch app.py
+#touch requirements.txt 
 touch research/trials.ipynb
-touch requirements.txt
+
 
 
 
